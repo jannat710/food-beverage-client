72cdf4644ee8c9f6577eac70c89e30bc18c07ad9
e <jannatul0040@gmail.com>
2023-10-18 22:42:07 +0600

    implement navbar and footer section

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar/Navbar";
+import Footer from "../components/shared/Footer/Footer";
 
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             
         </div>
     );
--- src/Routes/PublicRoute.jsx
+++ src/Routes/PublicRoute.jsx
@@ -1,6 +1,8 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
 import Home from "../components/Home/Home";
+import AddProduct from "../components/AddProduct/AddProduct";
+import MyCart from "../components/MyCart/MyCart";
   const router = createBrowserRouter([
     {
       path: "/",
@@ -9,6 +11,14 @@ import Home from "../components/Home/Home";
         {
             path:"/",
             element:<Home></Home>
+        },
+        {
+            path:"/addproduct",
+            element:<AddProduct></AddProduct>
+        },
+        {
+            path:"/mycart",
+            element:<MyCart></MyCart>
         }
       ]
     },
