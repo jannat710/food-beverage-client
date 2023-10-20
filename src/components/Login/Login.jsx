071cb6589f6768776dfff0f34994a766ce567210
e <jannatul0040@gmail.com>
2023-10-20 12:49:31 +0600

    modified login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import toast, { Toaster } from 'react-hot-toast';
 
 
 const Login = () => {
     const {googleSignIn,signIn}=useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
     console.log(googleSignIn)
+
+    const handleEmail = (text) => {
+        setEmail(text.target.value);
+        console.log(email)
+    }
+    const handlePassword = (text) => {
+        setPassword(text.target.value);
+        console.log(password)
+    }
 
     const handleGoogleSignIn=()=>{
         googleSignIn()
@@ -13,9 +26,23 @@ const Login = () => {
         });
     }
 
+    const handleLogin =()=>{
+        if(email,password){
+            signIn(email,password)
+            .then(result=>{
+                console.log(result.user)
+            })
+            .catch((err) => {
+                toast.error('Login Failed!');
+            })
+        }
+    }
+
 
     return (
-        <div className="hero min-h-screen bg-base-200">
+<div>
+    <Toaster></Toaster>
+<div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
     <div className="text-center lg:text-left">
       <h1 className="text-5xl font-bold">Login now!</h1>
@@ -26,19 +53,21 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input onChange={(e) => handleEmail(e)} type="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input onChange={(e) => handlePassword(e)} type="password" placeholder="password" className="input input-bordered" required />
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button onClick={handleLogin} className="btn btn-primary">Login</button>
           <button onClick={handleGoogleSignIn}>Google Sign In</button>
         </div>
+        
 
       </form>
     </div>
   </div>
+</div>
 </div>
     );
 };
