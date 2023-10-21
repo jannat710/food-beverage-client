72f7002c33e50f7aba9b2b00c5a862c21b70b8c9
e <jannatul0040@gmail.com>
2023-10-21 20:57:42 +0600

    modified brand name section

--- src/components/Home/Brands.jsx
+++ src/components/Home/Brands.jsx
@@ -3,7 +3,7 @@
 const Brands = ({ brands }) => {
     const { brand_name, image } = brands;
     return (
-        <div className="card max-w-7xl mx-auto bg-transparent border shadow-xl font-display">
+        <div className="card w-full mx-auto bg-transparent border shadow-xl font-display">
             <figure><img className="h-36 w-36" src={image} alt="car!" /></figure>
             <div className="card-body flex items-center justify-center">
                 <h2 className="card-title">{brand_name}</h2>
